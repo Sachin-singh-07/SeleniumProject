d503e4801354ee7414e1e831278d7421f2f4e6a5
sachin singh <137660244+Sachin-singh-07@users.noreply.github.com>
2024-03-05 11:00:19 +0530

    Add files via upload

---
+++ Contacts.java
@@ -0,0 +1,120 @@
+package VtigerCRM;
+
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import CommonUtils.ExcelUtil;
+import CommonUtils.ListnerImplementation;
+import CommonUtils.PropertyFileUtil;
+import CommonUtils.WebDriverUtil;
+
+@Listeners(ListnerImplementation.class)
+public class Contacts {
+
+	WebDriverUtil wutil = new WebDriverUtil();
+	PropertyFileUtil putil = new PropertyFileUtil();
+	ExcelUtil eutil = new ExcelUtil();
+
+	@Test
+	public void contacttest() throws IOException, InterruptedException {
+
+		WebDriver driver = new ChromeDriver();
+		wutil.maximize(driver);
+		wutil.implicitwait(driver);
+
+		String URL = putil.getDataFromPropertyFile("Url");
+		String USERNAME = putil.getDataFromPropertyFile("username");
+		String PASSWORD = putil.getDataFromPropertyFile("Password");
+
+		driver.get(URL);
+
+		// adding username
+		driver.findElement(By.name("user_name")).sendKeys(USERNAME);
+
+		// adding password
+		driver.findElement(By.name("user_password")).sendKeys(PASSWORD);
+
+		// to click on login button
+		driver.findElement(By.id("submitButton")).click();
+
+//		Click on contacts
+		driver.findElement(By.xpath("//a[text()='Contacts']")).click();
+
+		// To click on create contacts..(+)
+		driver.findElement(By.cssSelector("img[alt='Create Contact...']")).click();
+
+		// To read data from Excel File
+		Thread.sleep(2000);
+		String CONTACTFIRST = eutil.getDataFromExcel("Contact", 0, 1);
+		String CONTACTLAST = eutil.getDataFromExcel("Contact", 1, 1);
+		String CONGROUP = eutil.getDataFromExcel("Contact", 2, 1);
+		String ORGNAME = eutil.getDataFromExcel("Contact", 3, 1);
+
+		// to enter data from excel
+		Thread.sleep(2000);
+		driver.findElement(By.name("firstname")).sendKeys(CONTACTFIRST);
+		driver.findElement(By.name("lastname")).sendKeys(CONTACTLAST);
+
+//		// When we want to fail test script hear
+//		WebElement notificheckbox = driver.findElement(By.name("notify_owner"));
+//		Assert.assertTrue(notificheckbox.isSelected());
+
+		// In assigned to click on group
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("(//input[@name='assigntype'])[2]")).click();
+
+		// In the dropdown select Support Group WebElement condrop
+		Thread.sleep(2000);
+		driver.findElement(By.name("assigned_group_id"));
+
+		// manage dropdown
+		Thread.sleep(2000);
+		WebElement condrop = driver.findElement(By.name("assigned_group_id"));
+		wutil.HandleDropdowncontact(condrop, CONGROUP);
+
+		// Click on (+ symbol) to pass orginasization name Text field
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("(//img[@alt='Select'])[1]")).click();
+
+//		Transfer the driver control from Parent window to Child window
+		Thread.sleep(2000);
+		wutil.switchwindow(driver,
+				"http://localhost:8888/index.php?module=Accounts&action=Popup&popuptype=specific_contact_account_address&form=TasksEditView&form_submit=false&fromlink=&recordid=");
+
+		// To enter Organization Name in search
+		Thread.sleep(2000);
+		driver.findElement(By.id("search_txt")).sendKeys(ORGNAME);
+
+//		To click on Organization Name
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//a[text()='Qspider & Jspider']")).click();
+
+		// To transfer contol child to parent
+		wutil.switchwindow(driver,
+				"http://localhost:8888/index.php?module=Contacts&action=EditView&return_action=DetailView&parenttab=Marketing");
+
+//		// To take screenshot//		Thread.sleep(2000);
+//		wutil.screenshot(driver, "Contact");
+
+		// cLICK ON SAVE BUTTON
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("(//input[@name='button'])[1]")).click();
+
+		// Mouse hover on image WebElement image
+		Thread.sleep(2000);
+		WebElement image = driver.findElement(By.cssSelector("img[src='themes/softed/images/user.PNG']"));
+		wutil.mousehover(driver, image);
+
+		// Click on signout
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//a[text()='Sign Out']")).click();
+	}
+
+}
---
+++ Organization.java
@@ -0,0 +1,88 @@
+package VtigerCRM;
+
+import java.io.IOException;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import CommonUtils.ExcelUtil;
+import CommonUtils.JavaUtil;
+import CommonUtils.ListnerImplementation;
+import CommonUtils.PropertyFileUtil;
+import CommonUtils.WebDriverUtil;
+
+@Listeners(ListnerImplementation.class)
+public class Organization {
+
+	PropertyFileUtil putil = new PropertyFileUtil();
+	WebDriverUtil wutil = new WebDriverUtil();
+	ExcelUtil eutil = new ExcelUtil();
+	JavaUtil jutil = new JavaUtil();
+
+	@Test
+	public void OrganizationTest() throws IOException, InterruptedException {
+
+		WebDriver driver = new ChromeDriver();
+		wutil.maximize(driver);
+		wutil.implicitwait(driver);
+
+		// TO READ DATA FROM PROPERTY FILE
+		String URL = putil.getDataFromPropertyFile("Url");
+		String USERNAME = putil.getDataFromPropertyFile("username");
+		String PASSWORD = putil.getDataFromPropertyFile("Password");
+
+		// To read data from Excel File
+		String ORGNAME = eutil.getDataFromExcel("Organizations", 0, 1);
+		String GROUP = eutil.getDataFromExcel("Organizations", 1, 1);
+
+		// to launching the application
+		driver.get(URL);
+
+		// adding username
+		driver.findElement(By.name("user_name")).sendKeys(USERNAME);
+
+		// adding password
+		driver.findElement(By.name("user_password")).sendKeys(PASSWORD);
+
+		// to click on login button
+		driver.findElement(By.id("submitButton")).click();
+
+		// to click on orgination
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("(//a[text()='Organizations'])[1]")).click();
+
+		// to click on Create orgination
+		Thread.sleep(2000);
+		driver.findElement(By.cssSelector("img[alt='Create Organization...']")).click();
+
+		// to enter the compney(organizations) name
+		Thread.sleep(2000);
+		driver.findElement(By.name("accountname")).sendKeys(ORGNAME + jutil.getRandomNumber());
+
+		// In assigned to click on group
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("(//input[@name='assigntype'])[2]")).click();
+
+		// In the dropdown select Support Group WebElement dropdown
+		Thread.sleep(2000);
+		WebElement dropdown = driver.findElement(By.name("assigned_group_id"));
+		wutil.HandleDropdown(dropdown, GROUP);
+
+		// cLICK ON SAVE BUTTON
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("(//input[@name='button'])[1]")).click();
+
+		// Mouse hover on image WebElement image
+		Thread.sleep(2000);
+		WebElement image = driver.findElement(By.cssSelector("img[src='themes/softed/images/user.PNG']"));
+		wutil.mousehover(driver, image);
+
+		// Click on signout
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//a[text()='Sign Out']")).click();
+
+	}
+
+}
